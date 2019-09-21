1131015030ac94ff5a0d5854b5fdad0f2886ad2a
lcyanxi <2757710657@qq.com>
2019-09-21 23:30:22 +0800

    fix-bean

--- springboot-dubbo-demo/springboot-service/src/main/java/com/lcyanxi/service/OrderServiceImpl.java
+++ springboot-dubbo-demo/springboot-service/src/main/java/com/lcyanxi/service/OrderServiceImpl.java
@@ -3,10 +3,12 @@ package com.lcyanxi.service;
 
 import com.lcyanxi.dto.ResponseDTO;
 import com.lcyanxi.po.RequestPO;
+import org.springframework.stereotype.Service;
 
 /**
  * Created by lcyanxi on 2019/9/21
  */
+@Service("orderService")
 public class OrderServiceImpl implements IOrderService {
     public ResponseDTO doOrderNo(RequestPO po) {
         System.out.println("用户数据获取到" + po);
