9428addb9a67780bdb289aa9c6e9a036d06a1d08
lichang <lichang@koolearn.com>
2019-09-27 15:12:22 +0800

    xxl-job定时任务demo

---
+++ springboot-dubbo-demo/springboot-chronos/src/main/java/com/lcyanxi/chronos/SpringbootChronosApplication.java
@@ -0,0 +1,13 @@
+package com.lcyanxi.chronos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootChronosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootChronosApplication.class, args);
+    }
+
+}
---
+++ springboot-dubbo-demo/springboot-chronos/src/test/java/com/lcyanxi/chronos/SpringbootChronosApplicationTests.java
@@ -0,0 +1,16 @@
+package com.lcyanxi.chronos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SpringbootChronosApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
